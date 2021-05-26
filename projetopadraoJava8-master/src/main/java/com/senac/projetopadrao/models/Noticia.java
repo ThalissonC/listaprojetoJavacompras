d9dca531fed72f1649e3349951aca046f438d37f
thalisson nunes <thalisson.nunes@secult.local>
2021-05-26 17:46:08 -0300

    Initial commit

---
+++ projetopadraoJava8-master/src/main/java/com/senac/projetopadrao/models/Noticia.java
@@ -0,0 +1,68 @@
+package com.senac.projetopadrao.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Noticia implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String manchete;
+    private String introducao;
+    private String texto;
+    private String imagem;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getManchete() {
+        return manchete;
+    }
+
+    public void setManchete(String manchete) {
+        this.manchete = manchete;
+    }
+
+    public String getIntroducao() {
+        return introducao;
+    }
+
+    public void setIntroducao(String introducao) {
+        this.introducao = introducao;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+}

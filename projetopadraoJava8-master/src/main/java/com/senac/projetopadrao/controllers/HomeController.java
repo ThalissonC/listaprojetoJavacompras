7b7b235ed9170deeb92295933de354e37395148c
thalissonc <thalissontan13@gmail.com>
2021-06-07 21:24:44 -0300

    Conexão dos botões com o as páginas atualizadas

--- projetopadraoJava8-master/src/main/java/com/senac/projetopadrao/controllers/HomeController.java
+++ projetopadraoJava8-master/src/main/java/com/senac/projetopadrao/controllers/HomeController.java
@@ -1,19 +1,50 @@
 package com.senac.projetopadrao.controllers;
 
+import com.senac.projetopadrao.models.Alimento;
+import com.senac.projetopadrao.repositorys.AlimentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+
 @Controller
+
+
+
 public class HomeController {
+
+    @Autowired
+    AlimentoRepository alimentoRepository;
+
     @RequestMapping("/")
     public ModelAndView index(){
         return new ModelAndView("index");
     }
 
+    //@RequestMapping("/alimentos")
+    //public ModelAndView alimentos(){
+    //    return new ModelAndView("alimentos");
+    //}
+
     @RequestMapping("/alimentos")
-    public ModelAndView alimentos(){
-        return new ModelAndView("alimentos");
+    public ModelAndView listaAlimentos() {
+        ModelAndView mv = new ModelAndView("alimentos");
+
+        ArrayList<Alimento> alimentos = new ArrayList<>();
+
+        alimentos = (ArrayList<Alimento>) alimentoRepository.findAll();
+
+        mv.addObject("alimentos", alimentos);
+
+        return mv;
+    }
+
+    @GetMapping("/add")
+    public String addAlimentoPage(Alimento alimento) {
+        return "alimentos_add";
     }
 }
 

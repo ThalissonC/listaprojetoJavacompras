a6889823c0039b014373f42d3af2717b31189152
ThalissonC <65375127+ThalissonC@users.noreply.github.com>
2021-06-18 21:02:03 -0300

    funcionamento do edit

--- projetopadraoJava8-master/src/main/resources/static/js/main.js
+++ projetopadraoJava8-master/src/main/resources/static/js/main.js
@@ -20,22 +20,63 @@
 
 document.addEventListener("DOMContentLoaded" , function(event) {
 
+
     $('.table .btn').on('click', function(event){
         event.preventDefault();
 
         var id = $(this).attr('href').split('?')[1].split('=')[1];
 
-        const resp = $.getJSON(`/alimento/${id}`);
+        const resp = $.getJSON(`/api/alimentos/alimento/${id}`);
         resp.then( r => {
            console.log( r );
 
            $("#idEdit").val(r.id);
+           $("#nomeEdit").val(r.nome);
+           $("#quantidadeEdit").val(r.quantidade);
+           $("#dataEdit").val(r.data);
 
         });
 
         }
     )
  });
+
+
+ function updateAlimento (event){
+
+    const id = $("#idEdit").val();
+    const nome = $("#nomeEdit").val();
+    const quantidade = $("#quantidadeEdit").val();
+    const data = $("#dataEdit").val();
+
+    const dados = {"id":id, "nome":nome, "quantidade":quantidade, "data":data}
+
+    console.log(dados);
+
+
+    const apiUrl = `/api/alimentos/alimento/${id}`;
+
+    $.ajax({
+        contentType: "application/json; charset=uft-8",
+        url: apiUrl,
+        type: "PUT",
+        dataType: "json",
+        data: JSON.stringify(dados),
+        contentType: "application/json",
+        success: function(resp) {
+            console.log(resp);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+
+
+    })
+
+
+
+
+ }
 
 
 
